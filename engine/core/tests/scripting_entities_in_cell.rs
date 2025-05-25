91c3825e730b0c7c219c03d130ae5e5cc6435ce5
Vadim Titov <vadim.titov.hh@gmail.com>
2025-05-26 00:31:55 +0200

    test(scripting): add cell entity query test

---
+++ engine/core/tests/scripting_entities_in_cell.rs
@@ -0,0 +1,34 @@
+use engine_core::ecs::registry::ComponentRegistry;
+use engine_core::ecs::world::World;
+use engine_core::scripting::engine::ScriptEngine;
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+
+#[test]
+fn test_lua_entities_in_cell_api() {
+    let registry = Arc::new(Mutex::new(ComponentRegistry::new()));
+    let world = Rc::new(RefCell::new(World::new(registry.clone())));
+    {
+        let mut w = world.borrow_mut();
+        let mut grid = engine_core::map::SquareGridMap::new();
+        grid.add_cell(0, 0, 0);
+        grid.add_cell(1, 0, 0);
+        grid.add_neighbor((0, 0, 0), (1, 0, 0));
+        let map = engine_core::map::Map::new(Box::new(grid));
+        w.map = Some(map);
+    }
+
+    let mut engine = ScriptEngine::new();
+    engine.register_world(world.clone()).unwrap();
+
+    let lua_code = r#"
+        local eid = spawn_entity()
+        set_component(eid, "Position", { x = 0, y = 0, z = 0 })
+        local cell = { Square = { x = 0, y = 0, z = 0 } }
+        local entities = entities_in_cell(cell)
+        assert(#entities == 1, "Should find one entity in cell")
+    "#;
+
+    engine.run_script(lua_code).unwrap();
+}
