a4e63974a318e4146905219db9327ce86b99dfb0
Vadim Titov <vadim.titov.hh@gmail.com>
2025-06-05 22:26:44 +0200

    chore(python_api): remove duplicated methods

--- engine_py/src/python_api/economic.rs
+++ engine_py/src/python_api/economic.rs
@@ -1,6 +1,5 @@
 use super::PyWorld;
 use pyo3::prelude::*;
-use serde_pyobject::to_pyobject;
 
 pub trait EconomicApi {
     fn modify_stockpile_resource(&self, entity_id: u32, kind: String, delta: f64) -> PyResult<()>;
@@ -12,30 +11,5 @@ impl EconomicApi for PyWorld {
         world
             .modify_stockpile_resource(entity_id, &kind, delta)
             .map_err(pyo3::exceptions::PyValueError::new_err)
-    }
-}
-
-#[pymethods]
-impl PyWorld {
-    pub fn get_stockpile_resources(&self, entity_id: u32) -> PyResult<Option<PyObject>> {
-        let world = self.inner.borrow();
-        if let Some(stockpile) = world.get_component(entity_id, "Stockpile") {
-            if let Some(resources) = stockpile.get("resources") {
-                Python::with_gil(|py| Ok(Some(serde_pyobject::to_pyobject(py, resources)?.into())))
-            } else {
-                Ok(None)
-            }
-        } else {
-            Ok(None)
-        }
-    }
-
-    pub fn get_production_job(&self, entity_id: u32) -> PyResult<Option<PyObject>> {
-        let world = self.inner.borrow();
-        if let Some(job) = world.get_component(entity_id, "ProductionJob") {
-            Python::with_gil(|py| Ok(Some(serde_pyobject::to_pyobject(py, job)?.into())))
-        } else {
-            Ok(None)
-        }
     }
 }
