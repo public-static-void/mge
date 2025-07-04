08b76ce469e2f875afd821a6323d39f9c20a411f
Vadim Titov <vadim.titov.hh@gmail.com>
2025-07-04 23:51:57 +0200

    feat(job): add job_cancel API to Lua bridge

---
+++ engine_lua/src/lua_api/job_cancel.rs
@@ -0,0 +1,25 @@
+use engine_core::ecs::world::World;
+use mlua::{Lua, Result as LuaResult, Table};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub fn register_job_cancel_api(
+    lua: &Lua,
+    globals: &Table,
+    world: Rc<RefCell<World>>,
+) -> LuaResult<()> {
+    let world_cancel = world.clone();
+    let cancel_job = lua.create_function_mut(move |_lua, job_id: u32| {
+        let mut world = world_cancel.borrow_mut();
+        if let Some(mut job) = world.get_component(job_id, "Job").cloned() {
+            job["cancelled"] = serde_json::json!(true);
+            world
+                .set_component(job_id, "Job", job)
+                .map_err(mlua::Error::external)
+        } else {
+            Err(mlua::Error::external("Job not found"))
+        }
+    })?;
+    globals.set("cancel_job", cancel_job)?;
+    Ok(())
+}
