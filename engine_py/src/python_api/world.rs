abc055f252863be965b4bdb7f148176708185c6c
Vadim Titov <vadim.titov.hh@gmail.com>
2026-09-15 20:24:12 +0200

    feat(engine_py): register ConstructionSystem in PyWorld

--- engine_py/src/python_api/world.rs
+++ engine_py/src/python_api/world.rs
@@ -24,6 +24,7 @@ use crate::system_bridge::SystemBridge;
 use engine_core::ecs::world::World;
 use engine_core::loot::LootEntry;
 use engine_core::systems::body_part_damage::BodyPartDamageSystem;
+use engine_core::systems::construction::ConstructionSystem;
 use engine_core::systems::economic::{EconomicSystem, load_recipes_from_dir};
 use engine_core::systems::enemy_behavior::EnemyBehaviorSystem;
 use engine_core::systems::faction_reputation::FactionReputationSystem;
@@ -153,6 +154,7 @@ impl PyWorld {
         let recipes = load_recipes_from_dir(&recipes_dir);
         let economic_system = EconomicSystem::with_recipes(recipes);
         world.register_system(economic_system);
+        world.register_system(ConstructionSystem::new());
 
         Ok(PyWorld {
             inner: Rc::new(RefCell::new(world)),
